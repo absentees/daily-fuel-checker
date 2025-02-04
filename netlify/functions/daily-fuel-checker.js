b7192764c4684c2eeff72c3d22143501b06acf0c
Scott B <djlethal109@gmail.com>
2025-02-04 21:01:46 +1100

    testing

---
+++ netlify/functions/daily-fuel-checker.js
@@ -0,0 +1,17 @@
+import { schedule } from '@netlify/functions'
+
+
+exports.handler = async function (event, context) {
+	// Your logic to check fuel prices and perform necessary actions
+	console.log("Daily fuel checker function ran successfully.");
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: "Daily fuel checker function ran successfully." })
+	};
+};
+
+export const config = {
+	schedule: "@daily"
+}
+
